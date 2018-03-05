f775ddf002ce7a82bbbf78092d8688fd430627cc
Bharath Kamath <bharathkamath@Bharaths-MacBook-Air.local>
2018-03-04 22:22:18 -0500

    Added UI for the app - Next step to write Unit and UI tests

---
+++ TDDFizzBuzz/TDDFizzBuzzTests/ViewControllerUnitTests.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerUnitTests.swift
+//  TDDFizzBuzzTests
+//
+//  Created by Bharath Kamath on 2018-03-04.
+//  Copyright © 2018 Bharath Kamath. All rights reserved.
+//
+
+import XCTest
+@testable import TDDFizzBuzz
+
+class ViewControllerUnitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    
+}
